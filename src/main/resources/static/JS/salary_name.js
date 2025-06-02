16c00a57d381565522fa21622a21bbbaa8782ea5
Andyh03 <153629672+andyhRabelaza@users.noreply.github.com>
2025-06-02 23:34:39 +0300

    features_salary_et_f.paie

---
+++ src/main/resources/static/JS/salary_name.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Sélectionne toutes les lignes du tbody
+    const rows = document.querySelectorAll('tbody tr');
+
+    rows.forEach(row => {
+      row.addEventListener('click', () => {
+        // Récupère le contenu textuel de la 1ère cellule <td> (Name)
+        const name = row.querySelector('td').textContent.trim();
+        console.log("Nom du salaire cliqué :", name);
+      });
+    });
+  });
